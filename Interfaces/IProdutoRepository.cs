3239226ec6c31a62403d260cd637ff47939e27bb
Thiago Nunes <thiagonunesal1307@gmail.com>
2020-09-29 22:12:29 -0300

    Interface IProduto criada.

---
+++ Interfaces/IProdutoRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ORM_API_Loja.Interfaces
+{
+    interface IProdutoRepository
+    {
+    }
+}
